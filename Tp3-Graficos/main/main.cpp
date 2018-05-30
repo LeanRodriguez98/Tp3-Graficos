0e29cc855b4a41ac3315323cc38ab5dd5fd6278d
Unknown <jquintanamartin@hotmail.com>
2018-05-29 21:48:11 -0300

    Se muestra imagen

--- Tp3-Graficos/main/main.cpp
+++ Tp3-Graficos/main/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include "allegro5\allegro.h"
+#include "allegro5\allegro_image.h"
+#include "allegro5\allegro_native_dialog.h"
 
 int main(int argc, char **argv){
 
 	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_BITMAP  *image = NULL;
 
 	if (!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
 		return -1;
+	}
+
+	if (!al_init_image_addon()) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
 	}
 
 	display = al_create_display(640, 480);
@@ -16,13 +25,21 @@ int main(int argc, char **argv){
 		return -1;
 	}
 
-	al_clear_to_color(al_map_rgb(0, 0, 0));
+	image = al_load_bitmap("Personaje.png");
+	if (!image) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(display);
+		return 0;
+	}
+
+	al_draw_bitmap(image, 200, 200, 0);
 
 	al_flip_display();
-
 	al_rest(10.0);
 
 	al_destroy_display(display);
+	al_destroy_bitmap(image);
 
 	return 0;
 
